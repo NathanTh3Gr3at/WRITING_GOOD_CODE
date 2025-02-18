e11dc89906ca1c5a54f4d84bd389d7d9adc615f2
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2025-02-18 12:16:29 +0200

    Implement authentication and session management with CSRF protection; add validation utilities and error handling for user sessions

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const session = require('express-session');
-const mongodbStore = require('connect-mongodb-session');
 const csrf = require('csurf');
 
+const sessionConfig=require('./config/session')
+const authMiddleware=require('./middlewares/auth-middleware')
 const db = require('./data/database');
 const blogRoutes = require('./routes/blog');
+const authRoutes=require('./routes/auth')
+const addCSRFTokenMiddleware=require('./middlewares/csrf-token-middleware')
 
-const MongoDBStore = mongodbStore(session);
+const mongoDbSessionStore=sessionConfig.createSessionStore(session);
 
 const app = express();
 
-const sessionStore = new MongoDBStore({
-  uri: 'mongodb://localhost:27017',
-  databaseName: 'auth-demo',
-  collection: 'sessions'
-});
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -24,31 +23,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(session({
-  secret: 'super-secret',
-  resave: false,
-  saveUninitialized: false,
-  store: sessionStore,
-  cookie: {
-    maxAge: 2 * 24 * 60 * 60 * 1000
-  }
-}));
+app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
 app.use(csrf());
 
-app.use(async function(req, res, next) {
-  const user = req.session.user;
-  const isAuth = req.session.isAuthenticated;
+app.use(addCSRFTokenMiddleware);
+app.use(authMiddleware);
 
-  if (!user || !isAuth) {
-    return next();
-  }
-
-  res.locals.isAuth = isAuth;
-
-  next();
-});
-
+app.use(authRoutes)
 app.use(blogRoutes);
+
 
 app.use(function(error, req, res, next) {
   res.render('500');
